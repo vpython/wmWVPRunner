ef2d692c6d038c5d4b63537686e460f30038d0c8
c4snipes <124058651+c4snipes@users.noreply.github.com>
2023-11-15 01:49:39 +0000

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-	import { env } from '$env/dynamic/public'
+
 	import { stdoutStore } from '$lib/stores/stdoutSrc'
 	import { writable } from 'svelte/store'
 	import { setupGSCanvas, getPyodide } from '$lib/utils/utils'
 	import { onMount } from 'svelte'
+
 	let pyodide: any = null
 	let program: string
 	let stdout: HTMLTextAreaElement
@@ -12,108 +13,84 @@
 	let pyodideURL = 'https://cdn.jsdelivr.net/pyodide/v0.23.3/full/'
 	const screenshotUrl = writable('') //'https://cdn.jsdelivr.net/pyodide/v0.21.0a3/full/',
 
-	let defaultImportCode = `from math import *
+    let defaultImportCode = `from math import *
 from numpy import arange
 from random import random
 from vpython import *
 `
+    const substitutions: (RegExp | string)[][] = [
+        [/[^\.\w\n]rate[\ ]*\(/g, ' await rate('],
+        [/\nrate[\ ]*\(/g, '\nawait rate('],
+        [/\nscene\.waitfor[\ ]*\(/g, 'await scene.waitfor('],
+        [/[^\.\w\n]get_library[\ ]*\(/g, ' await get_library('],
+        [/\nget_library[\ ]*\(/g, '\nawait get_library(']
+    ];
 
-	const substitutions: (RegExp | string)[][] = [
-		[/[^\.\w\n]rate[\ ]*\(/g, ' await rate('],
-		[/\nrate[\ ]*\(/g, '\nawait rate('],
-		[/scene\.waitfor[\ ]*\(/g, 'await scene.waitfor('],
-		[/[^\.\w\n]get_library[\ ]*\(/g, ' await get_library('],
-		[/\nget_library[\ ]*\(/g, '\nawait get_library(']
-	]
+    function redirect_stdout(theText: string) {
+        if (mounted) {
+            stdoutStore.update((val: string) => (val += theText + '\n'));
+        }
+    }
 
-	function redirect_stdout(theText: string) {
-		if (mounted) {
-			stdoutStore.update((val: string) => (val += theText + '\n'))
-		}
+    function redirect_stderr(theText: string) {
+        if (mounted) {
+            stdoutStore.update((val: string) => (val += 'stderr:' + theText + '\n'));
+        }
+    }
+
+    onMount(async () => {
+        try {
+		
+            scene = await setupGSCanvas()
+            pyodide = await getPyodide(redirect_stdout, redirect_stderr, pyodideURL);
+
+            if (pyodide){
+                window.scene = scene; // @ts-ignore
+                window.__reportScriptError = (err) => {
+                    try {
+                        redirect_stderr('__reportScriptError:' + JSON.stringify(err));
+                    } catch (err) {
+                        redirect_stderr('__reportScriptError: Not sure! Cannot stringify');
+                    }
+                };
+
+                stdoutStore.set('');
+                mounted = true;
+
+                const trustedOrigins = [window.location.origin]; // Define trusted origins
+			
+                window.addEventListener('message', async (event) => {
+            // Security check: validate event.origin here if needed
+            if (event.data.action === 'captureScreenshot') {
+                captureScreenshot();
+            }
+		});
+	};
+	
+window.addEventListener('message', (e) => {
+	let obj = JSON.parse(e.data);
+	if (obj.program) {
+	let obj = JSON.parse(event.data);
+	if (obj.program) {
+		let program_lines = obj.program.split('\n');
+		program_lines[0] = '#' + program_lines[0]; // Comment out version string
+		program = program_lines.join('\n');
+		runMe();
 	}
+}
+})
 
-	function redirect_stderr(theText: string) {
-		if (mounted) {
-			stdoutStore.update((val: string) => (val += 'stderr:' + theText + '\n'))
-		}
-	}
 
-	onMount(async () => {
-		try {
-			scene = await setupGSCanvas()
-			pyodide = await getPyodide(redirect_stdout, redirect_stderr, pyodideURL)
 
-			if (pyodide) {
-				//@ts-ignore
-				window.scene = scene
-				//@ts-ignore
-				window.__reportScriptError = (err) => {
-					try {
-						redirect_stderr('__reportScriptError:' + JSON.stringify(err))
-					} catch (err) {
-						redirect_stderr('__reportScriptError: Not sure! Cannot stringify')
-					}
-					debugger
-				}
-
-				stdoutStore.set('')
-				mounted = true
-
-				window.addEventListener('message', (e) => {
-					console.log('In window message:' + JSON.parse(e.data))
-					let obj = JSON.parse(e.data)
-					if (obj.program) {
-						let program_lines = obj.program.split('\n')
-						program_lines[0] = '#' + program_lines[0]
-						program = program_lines.join('\n')
-						runMe()
-					}
-				})
-
-				try {
-					await captureScreenshot(false, scene)
-				} catch (error) {
-					console.error('Error capturing screenshot:', error)
-				}
-			} else {
-				redirect_stderr('Pyodide not found')
-			}
-		} catch (e) {
-			redirect_stderr(JSON.stringify(e))
-		}
-
-		return () => {
-			mounted = false
-		}
-	})
-
-	async function captureScreenshot(
-		isAuto: boolean,
-		scene: { __renderer: { screenshot: () => Promise<HTMLCanvasElement> } }
-	) {
-		const canvasElement = document.getElementById('glowscript')
-		if (canvasElement instanceof HTMLCanvasElement) {
-			var img = await scene.__renderer.screenshot()
-			var targetSize = 128
-			var aspect = img.width / img.height
-			var w = aspect >= 1 ? targetSize : targetSize * aspect
-			var h = aspect >= 1 ? targetSize / aspect : targetSize
-			var cvs = document.createElement('canvas')
-			cvs.width = w
-			cvs.height = h
-			var cx = cvs.getContext('2d')
-			if (cx !== null) {
-				cx.drawImage(img, 0, 0, w, h)
-				var thumbnail = cvs.toDataURL()
-				localStorage.setItem('captureScreenshot', JSON.stringify({ thumbnail, isAuto }));
-			} else {
-				console.error('Could not get the 2D context from the canvas')
-			}
-		} else {
-			console.error('The element with id "glowscript" is not a canvas')
-		}
-	}
-
+    function captureScreenshot() {
+        const canvasElement = document.getElementById('glowscript');
+        if (canvasElement instanceof HTMLCanvasElement) {
+            const imageDataUrl = canvasElement.toDataURL('image/png');
+            screenshotUrl.set(imageDataUrl); // Update the Svelte store with the screenshot data URL
+        } else {
+            console.error('The element is not a canvas');
+        }
+    }
 	async function runMe() {
 		try {
 			if (pyodide) {
