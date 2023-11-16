f9e6196609acb2a4d3f970736531daf135e1c2ba
c4snipes <124058651+c4snipes@users.noreply.github.com>
2023-11-16 14:45:41 +0000

    format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-
 	import { stdoutStore } from '$lib/stores/stdoutSrc'
 	import { get, writable } from 'svelte/store'
 	import { setupGSCanvas, getPyodide } from '$lib/utils/utils'
@@ -13,101 +12,99 @@
 	let pyodideURL = 'https://cdn.jsdelivr.net/pyodide/v0.23.3/full/'
 	const screenshotUrl = writable('') //'https://cdn.jsdelivr.net/pyodide/v0.21.0a3/full/',
 
-    let defaultImportCode = `from math import *
+	let defaultImportCode = `from math import *
 from numpy import arange
 from random import random
 from vpython import *
 `
-    const substitutions: (RegExp | string)[][] = [
-        [/[^\.\w\n]rate[\ ]*\(/g, ' await rate('],
-        [/\nrate[\ ]*\(/g, '\nawait rate('],
-        [/\nscene\.waitfor[\ ]*\(/g, 'await scene.waitfor('],
-        [/[^\.\w\n]get_library[\ ]*\(/g, ' await get_library('],
-        [/\nget_library[\ ]*\(/g, '\nawait get_library(']
-    ];
+	const substitutions: (RegExp | string)[][] = [
+		[/[^\.\w\n]rate[\ ]*\(/g, ' await rate('],
+		[/\nrate[\ ]*\(/g, '\nawait rate('],
+		[/\nscene\.waitfor[\ ]*\(/g, 'await scene.waitfor('],
+		[/[^\.\w\n]get_library[\ ]*\(/g, ' await get_library('],
+		[/\nget_library[\ ]*\(/g, '\nawait get_library(']
+	]
 
-    function redirect_stdout(theText: string) {
-        if (mounted) {
-            stdoutStore.update((val: string) => (val += theText + '\n'));
-        }
-    }
+	function redirect_stdout(theText: string) {
+		if (mounted) {
+			stdoutStore.update((val: string) => (val += theText + '\n'))
+		}
+	}
 
-    function redirect_stderr(theText: string) {
-        if (mounted) {
-            stdoutStore.update((val: string) => (val += 'stderr:' + theText + '\n'));
-        }
-    }
+	function redirect_stderr(theText: string) {
+		if (mounted) {
+			stdoutStore.update((val: string) => (val += 'stderr:' + theText + '\n'))
+		}
+	}
 
-    onMount(() => {
-        const trustedOrigins = [window.location.origin]; // Define trusted origins
+	onMount(() => {
+		const trustedOrigins = [window.location.origin] // Define trusted origins
 
-        window.addEventListener('message', (event) => {
-            let obj = JSON.parse(event.data)
-            if (obj.program) {
-					let program_lines = obj.program.split('\n') // comment out version string... keep line numbers the same
-					program_lines[0] = '#' + program_lines[0]
-					program = program_lines.join('\n')
-					runMe()
-				}
-        else if (!trustedOrigins.includes(event.origin)) {
-                console.error('Received message from untrusted origin:', event.origin);
-                return;
-            }
+		window.addEventListener('message', (event) => {
+			let obj = JSON.parse(event.data)
+			if (obj.program) {
+				let program_lines = obj.program.split('\n') // comment out version string... keep line numbers the same
+				program_lines[0] = '#' + program_lines[0]
+				program = program_lines.join('\n')
+				runMe()
+			} else if (!trustedOrigins.includes(event.origin)) {
+				console.error('Received message from untrusted origin:', event.origin)
+				return
+			}
 
-            const messageData = JSON.parse(event.data);
-            if (messageData.action === 'captureScreenshot') {
-                captureScreenshot(scene);
-                runMe();
-            }
-        });
-    });
-	async function captureScreenshot(
-    scene: { __renderer: { screenshot: () => Promise<HTMLCanvasElement> } }
-) {
-    try {
-        // Ensure the canvas element is correctly identified
-        const canvasElement = document.getElementById('glowscript');
-        if (!(canvasElement instanceof HTMLCanvasElement)) {
-            console.error('The element with id "glowscript" is not a canvas');
-            return;
-        }
+			const messageData = JSON.parse(event.data)
+			if (messageData.action === 'captureScreenshot') {
+				captureScreenshot(scene)
+				runMe()
+			}
+		})
+	})
+	async function captureScreenshot(scene: {
+		__renderer: { screenshot: () => Promise<HTMLCanvasElement> }
+	}) {
+		try {
+			// Ensure the canvas element is correctly identified
+			const canvasElement = document.getElementById('glowscript')
+			if (!(canvasElement instanceof HTMLCanvasElement)) {
+				console.error('The element with id "glowscript" is not a canvas')
+				return
+			}
 
-        // Capture the screenshot using the provided method
-        const img = await scene.__renderer.screenshot();
-        if (!img) {
-            console.error('Failed to capture the screenshot from the scene');
-            return;
-        }
+			// Capture the screenshot using the provided method
+			const img = await scene.__renderer.screenshot()
+			if (!img) {
+				console.error('Failed to capture the screenshot from the scene')
+				return
+			}
 
-        // Define the target size for the screenshot
-        const targetSize = 128;
-        const aspectRatio = img.width / img.height;
-        const width = aspectRatio >= 1 ? targetSize : targetSize * aspectRatio;
-        const height = aspectRatio >= 1 ? targetSize / aspectRatio : targetSize;
+			// Define the target size for the screenshot
+			const targetSize = 128
+			const aspectRatio = img.width / img.height
+			const width = aspectRatio >= 1 ? targetSize : targetSize * aspectRatio
+			const height = aspectRatio >= 1 ? targetSize / aspectRatio : targetSize
 
-        // Create a new canvas to adjust the screenshot size
-        const screenshotCanvas = document.createElement('canvas');
-        screenshotCanvas.width = width;
-        screenshotCanvas.height = height;
+			// Create a new canvas to adjust the screenshot size
+			const screenshotCanvas = document.createElement('canvas')
+			screenshotCanvas.width = width
+			screenshotCanvas.height = height
 
-        const context = screenshotCanvas.getContext('2d');
-        if (!context) {
-            console.error('Could not get the 2D context from the canvas');
-            return;
-        }
+			const context = screenshotCanvas.getContext('2d')
+			if (!context) {
+				console.error('Could not get the 2D context from the canvas')
+				return
+			}
 
-        // Draw the image on the new canvas
-        context.drawImage(img, 0, 0, width, height);
-        const thumbnail = screenshotCanvas.toDataURL();
+			// Draw the image on the new canvas
+			context.drawImage(img, 0, 0, width, height)
+			const thumbnail = screenshotCanvas.toDataURL()
 
-        // Store the captured screenshot in local storage
-        localStorage.setItem('captureScreenshot', JSON.stringify({ thumbnail, isAuto: false }));
+			// Store the captured screenshot in local storage
+			localStorage.setItem('captureScreenshot', JSON.stringify({ thumbnail, isAuto: false }))
+		} catch (error) {
+			console.error('Error capturing screenshot:', error)
+		}
+	}
 
-    } catch (error) {
-        console.error('Error capturing screenshot:', error);
-    }
-}
-	
 	async function runMe() {
 		try {
 			if (pyodide) {
