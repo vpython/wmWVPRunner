17d3ab2072ac7f6fc6e3c889f37a0121ba2e9b68
Steve Spicklemire <steve@spvi.com>
2026-06-13 15:28:17 -0400

    fix: use Vite plugin to set COOP/COEP headers
    - Create custom plugin to configure server middleware
    - Set headers on all requests for SharedArrayBuffer support

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,20 @@ import { defineConfig } from 'vite'
 import { sveltekit } from '@sveltejs/kit/vite'
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		middlewareMode: false,
-		middleware: [
-			(req, res, next) => {
-				// Set COOP/COEP headers for SharedArrayBuffer support (Web Worker synchronization)
-				res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-				res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-				next();
+	plugins: [
+		{
+			name: 'add-headers',
+			configureServer(server) {
+				return () => {
+					server.middlewares.use((req, res, next) => {
+						// Set COOP/COEP headers for SharedArrayBuffer support (Web Worker synchronization)
+						res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+						res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+						next();
+					});
+				};
 			}
-		]
-	}
+		},
+		sveltekit()
+	]
 });
